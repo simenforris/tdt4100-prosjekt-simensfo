65b1134ec2348358aad848c5d20aed3c5d0ae284
simenforris <simenforris@gmail.com>
2021-03-21 23:18:42 +0000

    Added title to window

--- src/main/java/poker/fxui/App.java
+++ src/main/java/poker/fxui/App.java
@@ -1,11 +1,24 @@
 package poker.fxui;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-public class App extends Application
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App extends Application {
+
+	@Override
+	public void start(Stage stage) throws Exception {
+		Parent parent = FXMLLoader.load(getClass().getResource("Poker.fxml"));
+		
+		Scene scene = new Scene(parent);
+
+		stage.setTitle("War Poker");
+		stage.setScene(scene);
+		stage.show();
+	}
+    public static void main(String[] args) {
+        launch(App.class, args);
     }
 }
